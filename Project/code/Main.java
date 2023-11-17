6f92117584ce23f11b77e783323abdca6b1df394
Syed Afridi <114821886+SyedAfridi6@users.noreply.github.com>
2023-11-17 12:10:01 +0530

    Update Main.java

--- Project/code/Main.java
+++ Project/code/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Continuous Integration! and continous devlopment!!");
+        System.out.println("Hello, Continuous Integration! and continous hello");
     }
 }
